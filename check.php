9528ba508ab301de1a5f5de10d5acf0c0f8e0a41
Paul Reed <paul.reed@puppet.com>
2021-03-11 14:04:04 -0500

    add google api stuff

--- check.php
+++ check.php
@@ -4,10 +4,30 @@
 $config = json_decode(file_get_contents('config.json'),true);
 require_once 'vendor/autoload.php';
 
-    
+function getGClient($credfile='credentials.json')
+{
+    $client = new Google_Client();
+    putenv('GOOGLE_APPLICATION_CREDENTIALS=./'.$credfile);
+    $client->useApplicationDefaultCredentials();
+    $client->setApplicationName('jarvis.se.puppet.net');
+    $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+    return $client;
+}
+
+$client = getGClient("google_credentials.json");
+$service = new Google_Service_Sheets($client);
+
+// Sheet info
+$range='Sheet1!A2:AAA';
+$response = $service->spreadsheets_values->get($config['google_sheet_id'], $range);
+$rows = $response->getValues();
+
+foreach($rows as $row) {
+    echo $row[0]."\n";
+}
 
 
-print_r($config);
+//print_r($config);
 
 
 
