79e29d2f827cfc1cec59d1c5d8209650d085781e
Paul Reed <paul.reed@puppet.com>
2021-03-11 13:46:55 -0500

    updates

--- check.php
+++ check.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php
 <?php
 
-    $config = json_decode(file_get_contents('config.json'),true);
+$config = json_decode(file_get_contents('config.json'),true);
+require_once 'vendor/autoload.php';
+
     
 
 
-    print_r($config);
+print_r($config);
 
 
 
-    echo "\n";
+echo "\n";
 
 ?>
