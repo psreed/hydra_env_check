b6e6782abf3e44699fc878df4940b04b1a17e9ed
Paul Reed <paul.reed@puppet.com>
2021-03-11 14:32:08 -0500

    .

--- check.php
+++ check.php
@@ -26,7 +26,7 @@ function update_sheet_cell($service, $google_sheet_id, $range, $val) {
         'values' => $values
     ]);
     $params = [
-        'valueInputOption' => $valueInputOption
+//        'valueInputOption' => $valueInputOption
     ];
     $result = $service->spreadsheets_values->update($google_sheet_id, $range, $body, $params);
     print_r($result);
