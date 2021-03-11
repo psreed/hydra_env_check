ae3528c3f5638774f91550ef0eadd938e61dfe16
Paul Reed <paul.reed@puppet.com>
2021-03-11 14:59:30 -0500

    .

--- check.php
+++ check.php
@@ -37,7 +37,8 @@ function update_sheet_cell($google_sheet_id, $range, $val) {
 function curl_check($url) {
     echo "Checking URL: ".$url."\n";
     $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_MUTE, 1);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_VERBOSE, false);
     $info=curl_exec($ch);
     $info=curl_getinfo($ch);
     return ($info);
